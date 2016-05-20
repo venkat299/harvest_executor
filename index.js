29bf0fb24633eecc676154700a527715322005b2
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2016-05-20 17:44:02 +0530

    added buy function

---
+++ index.js
@@ -0,0 +1,21 @@
+var executor = require('./lib/order_executor.js');
+
+var opts = {};
+
+module.exports = function(options) {
+
+	var seneca = this
+	var extend = seneca.util.deepextend
+	opts = extend(opts, options)
+
+	this.executor_config = options
+	seneca.add('role:executor,cmd:place_order', executor.place_order)
+	//seneca.add('role:executor,cmd:sell', executor.sell)
+		//seneca.add('role:info,req:part', aliasGet)
+
+	return {
+		name: 'harvest_executor'
+	}
+
+
+}
